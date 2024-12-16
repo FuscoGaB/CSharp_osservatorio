8ba643c68fce41036ebbadfd3de4819a1aff176d
FuscoGaB <fusco.gabriele007@gmail.com>
2024-12-16 17:43:52 +0100

    codice incorretto

---
+++ osservatorio/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace osservatorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btn_start = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // btn_start
+            // 
+            btn_start.Location = new Point(293, 121);
+            btn_start.Name = "btn_start";
+            btn_start.Size = new Size(94, 29);
+            btn_start.TabIndex = 0;
+            btn_start.Text = "inizia";
+            btn_start.UseVisualStyleBackColor = true;
+            btn_start.Click += btn_start_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_start);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
